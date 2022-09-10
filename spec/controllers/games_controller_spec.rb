795fc12c7447df7a640c684a0d4caa47a80cfb83
Clyde <dimich_vlg@mail.ru>
2022-09-10 21:04:21 +0300

    User gave wrong answer controller tests updated

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -162,21 +162,24 @@ RSpec.describe GamesController, type: :controller do
 
     context 'gave wrong answer and' do
       let!(:answer_key) do 
-        ["a", "b", "c", "d"].grep_v(game_w_questions.current_game_question.correct_answer_key).sample
+        %w[a b c d].grep_v(game_w_questions.current_game_question.correct_answer_key).sample
       end
 
       before do
         put :answer, id: game_w_questions.id, letter: answer_key
+        @game = assigns(:game)
       end
 
       it 'game was over' do
-        game = assigns(:game)
-        expect(game.finished?).to be true
+        expect(@game.finished?).to be true
+      end
+
+      it 'game finishes with status fail' do
+        expect(@game.status).to eq(:fail)
       end
 
       it 'was redirected to his profile' do
-        game = assigns(:game)
-        expect(response).to redirect_to(user_path(game.user))
+        expect(response).to redirect_to(user_path(@game.user))
       end
 
       it 'got flash message' do
